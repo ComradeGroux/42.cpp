fe998830edd41bb060a98381bae6b4cc26036cc2
Victor Groux <victorgroux99@gmail.com>
2023-04-10 13:05:48 +0200

    ex01 OK

--- cpp01/ex01/srcs/main.cpp
+++ cpp01/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 13:47:15 by vgroux            #+#    #+#             */
-/*   Updated: 2023/04/04 14:09:00 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/04/10 13:04:23 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,27 @@
 
 int	main(void)
 {
-	Zombie	z1 = Zombie("Foo");
-	Zombie	z2 = Zombie(z1);
+	int		n = 5;
+	Zombie* horde = NULL;
 
-	randomChump("Random name");
-	z2.announce();
-	Zombie* z3 = newZombie("newOne");
-	z3->announce();
-	delete(z3);
+	std::cout << std::endl << "horde de " << n << " membres" << std::endl;
+	horde = zombieHorde(n, "foo");
+	for (int i = 0; i < n; i++)
+		horde[i].announce();
+	delete[] (horde);
+
+	n = -42;
+	std::cout << std::endl << "horde de " << n << " membres" << std::endl;
+	horde = zombieHorde(n, "foo");
+	for (int i = 0; i < n; i++)
+		horde[i].announce();
+	delete[] (horde);
+
+	n = 2;
+	std::cout << std::endl << "horde de " << n << " membres" << std::endl;
+	horde = zombieHorde(n, "foo");
+	for (int i = 0; i < n; i++)
+		horde[i].announce();
+	delete[] (horde);
 	return (0);
 }
--- cpp01/ex01/srcs/zombieHorde.cpp
+++ cpp01/ex01/srcs/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:12:00 by vgroux            #+#    #+#             */
-/*   Updated: 2023/04/04 14:12:41 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/04/10 13:01:13 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+		return (NULL);
 
+	Zombie*	horde = new (std::nothrow) Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].setName(name);
+	}
+	return (horde);
 }
