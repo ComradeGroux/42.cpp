572d33e956c6807cb40f3f9218b9a252ec0e3119
Victor Groux <victorgroux99@gmail.com>
2023-04-17 21:25:09 +0200

    correction gestion erreur ex04

--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -6,13 +6,32 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 17:37:05 by vgroux            #+#    #+#             */
-/*   Updated: 2023/04/13 18:51:50 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/04/17 21:24:39 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 #include <fstream>
+
+std::string	strReplace(std::string line, std::string const s1, std::string const s2)
+{
+	std::string	out;
+	size_t		start;
+	size_t		end;
+
+	start = 0;
+	end = line.find(s1);
+	while (end != std::string::npos)
+	{
+		out.append(line, start, end - start);
+		out.append(s2);
+		start = end - s1.size();
+		end = line.find(s1, start);
+	}
+	out.append(s2, start);
+	return (out);
+}
 
 int	main(int argc, char** argv)
 {
@@ -27,9 +46,11 @@ int	main(int argc, char** argv)
 	std::string		s1 = argv[2];
 	std::string		s2 = argv[3];
 	std::string		line = argv[1];
-	size_t			found;
 	if (s1.empty() || s2.empty() || line.empty())
+	{
 		std::cerr << "Error: empty arguments" << std::endl;
+		return (1);
+	}
 	baseFile.open(line.c_str(), std::fstream::out);
 	if (baseFile.is_open())
 	{
@@ -37,24 +58,20 @@ int	main(int argc, char** argv)
 		if (newFile.is_open())
 		{
 			while (getline(baseFile, line))
-			{
-				found = line.find(s1);
-				while (found != std::string::npos)
-				{
-					line.erase(found, found + s1.length());
-					line.insert(found, s2);
-					found = line.find(s1);
-				}
-				newFile << line << std::endl;
-			}
+				newFile << strReplace(line, s1, s2) << std::endl;
 			newFile.close();
 		}
 		else
 		{
 			std::cerr << "The file called <" << argv[1] << ".replace doesn't exist." << std::endl;
+			return (1);
 		}
 		baseFile.close();
 	}
 	else
+	{
 		std::cerr << "The file called <" << argv[1] << " doesn't exist." << std::endl;
+		return (1);
+	}
+	return (0);
 }
