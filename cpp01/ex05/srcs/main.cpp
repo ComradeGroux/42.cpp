fc51fbed1e570542bcfb4223a8135ce8bc2a958a
Victor Groux <victorgroux99@gmail.com>
2023-04-19 16:34:01 +0200

    main OK

--- cpp01/ex05/srcs/main.cpp
+++ cpp01/ex05/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 21:51:59 by vgroux            #+#    #+#             */
-/*   Updated: 2023/04/17 21:52:23 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/04/19 16:33:09 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 int	main(void)
 {
 	Harl	test;
+	test.complain("ERROR");
+	test.complain("WARNING");
+	test.complain("INFO");
+	test.complain("DEBUG");
+	std::cout << std::endl << "Rien devrait s'afficher ensuite car le level n'est pas existant" << std::endl;
+	test.complain("warning");
+	test.complain("error");
 	return (0);
 }
