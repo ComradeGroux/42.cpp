12529d9b512b49827a6718503eaf0cd55926a140
Victor Groux <victorgroux99@gmail.com>
2023-04-19 19:01:28 +0200

    main

---
+++ cpp02/ex00/srcs/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:51:59 by vgroux            #+#    #+#             */
+/*   Updated: 2023/04/19 18:59:29 by vgroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
