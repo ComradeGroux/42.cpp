0a2d22f4d77c1c595ba7d5fd8b6674bd42a661b9
Victor Groux <victorgroux99@gmail.com>
2023-04-20 22:28:38 +0200

    nik

--- cpp02/ex02/srcs/Fixed.cpp
+++ cpp02/ex02/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 21:30:25 by vgroux            #+#    #+#             */
-/*   Updated: 2023/04/20 22:25:15 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/04/20 22:27:40 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,35 @@
 
 Fixed::Fixed(void): _value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed(const int n)
 {
-	std::cout << "Int constructor called" << std::endl;
 	_value = n << _nbBits;
 	return ;
 }
 
 Fixed::Fixed(const float n)
 {
-	std::cout << "Float constructor called" << std::endl;
 	_value = roundf(n * (1 << _nbBits));
 	return ;
 }
 
 Fixed::Fixed(const Fixed& src)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 	return ;
 }
 
 Fixed&	Fixed::operator=(const Fixed& rhs)
 {
-	std::cout << "Copy assignement operator called" << std::endl;
 	setRawBits(rhs.getRawBits());
 	return *this;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
 	return ;
 }
 
