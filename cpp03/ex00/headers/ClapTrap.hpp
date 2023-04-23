785a6b1f42321510a404d76bbe74cdfe507b2fa1
Victor Groux <victorgroux99@gmail.com>
2023-04-23 16:15:37 +0200

    ex00 main a finir

---
+++ cpp03/ex00/headers/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 22:41:28 by vgroux            #+#    #+#             */
+/*   Updated: 2023/04/23 16:45:46 by vgroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hp;
+		int			_ep;
+		int			_ad;
+
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& src);
+		ClapTrap& operator=(const ClapTrap& src);
+		~ClapTrap(void);
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
