2a638d03626fb9b4d1c94149eaf4dc4ab13dabc7
Victor Groux <victorgroux99@gmail.com>
2023-12-07 18:07:44 +0100

    correction swap to take address to effectively swap the data

--- cpp07/ex00/headers/templates.hpp
+++ cpp07/ex00/headers/templates.hpp
@@ -6,14 +6,16 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:58:24 by vgroux            #+#    #+#             */
-/*   Updated: 2023/12/07 18:02:29 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/12/07 18:06:34 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+# include <iostream>
+
 template <typename T>
-void	swap(T a, T b)
+void	swap(T& a, T& b)
 {
 	T	c = a;
 	a = b;
