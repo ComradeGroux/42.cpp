06ffa9525e129010c5f0a33f335a3e98c73b8c8a
Victor Groux <victorgroux99@gmail.com>
2023-12-07 18:12:54 +0100

    ex01 commence

---
+++ cpp07/ex01/headers/iter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 18:08:35 by vgroux            #+#    #+#             */
+/*   Updated: 2023/12/07 18:11:34 by vgroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void	iter(T* array, size_t len, void (*f)(const T&))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		f(array[i]);
+	}
+}
